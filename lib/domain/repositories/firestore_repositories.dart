be1641f247ff9d709f25bb1a685d3a2539c0bdbf
chernov <sergeychernov2801@gmail.com>
2024-02-24 21:28:28 +0200

    first commit

--- lib/domain/repositories/firestore_repositories.dart
+++ lib/domain/repositories/firestore_repositories.dart
@@ -4,4 +4,5 @@ import 'package:testing_app/data/models/user_model.dart';
 abstract class FirestoreRepositories {
   Future<UserModel> getData({required UserParams params});
   Future<void> recordData({required UserParams params});
+  
 }
